bb8c32e1963122fc736ca912abec389fde9cf01e
Edward Chen <echen333us@gmail.com>
2025-09-01 22:37:48 -0400

    jko, code clean

---
+++ README.md
@@ -0,0 +1,8 @@
+# Flow generative models
+
+- [JKO-iFlow](https://github.com/hamrel-cxu/JKO-iFlow)
+- [Flow-OT](https://github.com/hamrel-cxu/FlowOT)
+
+<img src="./img_rose.png" alt="rose image" width="200"/>
+
+<img src="./assets/rose.png" alt="constructed rose" width="200"/>
--- jko_tests.py
+++ jko_tests.py
@@ -1,26 +1,33 @@
 import torch
-import torch.nn as nn
-import os
-import yaml
-from flow import JKO, ODEFuncBlock, NN, sample_points_from_image, reparameterize_trajectory
+from jko_iflow import (
+    JKO,
+    ODEFuncBlock,
+    NN,
+)
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
+
 
 def test_forward():
     pass
 
+
 def main():
     save_path = "train_1"
-    device = ("cuda" if torch.cuda.is_available() else "cpu")
-    sdict = torch.load(f"chkpt/{save_path}.pth")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    sdict = torch.load(f"chkpt/{save_path}.pth", map_location=device)
     args = sdict["args"]
     h_ks = sdict["h_ks"]
-    # h_ks =[np.float64(0.08704150759150331), np.float64(0.1831668147369398), np.float64(0.5342127305485939), np.float64(1.4043969599112267), np.float64(2.8153883490293747), np.float64(5.0), np.float64(5.0), np.float64(5.0)]
 
     flow = JKO(
         [
             ODEFuncBlock(
-                h_ks[idx], NN(device), args["sigma_0"] / args["d"], ode_solver="rk4", device=device
+                h_ks[idx],
+                NN(device),
+                args["sigma_0"] / np.sqrt(args["d"]),
+                ode_solver="rk4",
+                device=device,
             )
             for idx in range(args["num_blocks"])
         ]
@@ -33,10 +40,20 @@ def main():
     with torch.no_grad():
         for idx, block in reversed(list(enumerate(flow.blocks))):
             p_z = points
-            
-            plt.scatter(x=p_z[:, 0].cpu().detach().numpy(), y=p_z[:, 1].cpu().detach().numpy(), s=5, alpha=0.3)
+
+            x = p_z[:, 0].cpu().detach().numpy()
+            y = p_z[:, 1].cpu().detach().numpy()
+            # sns.kdeplot(x=x, y=y, fill=True, cmap="mako", thresh=0.001, bw_adjust=0.3)
+            plt.show()
+
+            plt.scatter(
+                x=x,
+                y=y,
+                s=5,
+                alpha=0.3,
+            )
             plt.title(f"block idx: {idx}")
-            plt.savefig(f'plot_{idx}.png')
+            plt.savefig(f"plot_{idx}.png")
             plt.close()
             p_x, _, _ = block(points, reverse=True)
 
